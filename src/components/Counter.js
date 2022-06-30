2aff03e34df01dfff1fb07427cd220fa6fb9e136
Rafabc18 <70495348+growebux@users.noreply.github.com>
2022-06-30 13:09:43 -0700

    Increase the counter by 5 and hide the value when Togger is clicked

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,12 @@ import { useSelector, useDispatch } from "react-redux";
 const CounterComponent = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showContent);
 
   const incrementCounter = () => {
     dispatch({ type: "INCREMENT" });
   };
+
   const decrementCounter = () => {
     dispatch({ type: "DECREMENT" });
   };
@@ -15,14 +17,18 @@ const CounterComponent = () => {
     dispatch({ type: "INCREASE", amount: 5 });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "HIDE" });
+  };
+
   return (
     <div className="counter">
       <h1>Redux</h1>
-      <div className="value">{counter}</div>
+      {show && <div className="value">{counter}</div>}
+
       <button onClick={incrementCounter}>+</button>
       <button onClick={decrementCounter}>_</button>
-      <button onClick={increaseCounter}>incrementByFive</button>
+      <button onClick={increaseCounter}>Increment By 5</button>
       <button onClick={toggleCounterHandler}>Togger Counter</button>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,34 @@
 import { createStore } from "redux";
 
-const counterReducer = function (state = { counter: 0 }, action) {
+const initialState = { counter: 0, showContent: true };
+
+const counterReducer = function (state = initialState, action) {
   if (action.type === "INCREMENT")
     return {
       counter: state.counter + 1,
+      showContent: state.showContent,
     };
 
   if (action.type === "INCREASE") {
-    return { counter: state.counter + action.amount };
+    return {
+      counter: state.counter + action.amount,
+      showContent: state.showContent,
+    };
   }
   if (action.type === "DECREMENT") {
     return {
       counter: state.counter - 1,
+      showContent: state.showContent,
     };
   }
+
+  if (action.type === "HIDE") {
+    return {
+      showContent: !state.showContent,
+      counter: state.counter,
+    };
+  }
+
   return state;
 };
 
